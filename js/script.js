2b8509721e5cd6831c4c614f31c5756f89266310
Thinkworks <s.van.groenendaal@sintlucasedu.nl>
2023-10-31 09:00:22 +0100

    Before last revision

--- js/script.js
+++ js/script.js
@@ -18,3 +18,18 @@ function getQuote() {
     function googleTranslateElementInit() {
     new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
 }
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
